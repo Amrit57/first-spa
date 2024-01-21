4064bd840bfe511c0f323e406d6abad78268053e
Amrit <fenpadorje7@gmail.com>
2024-01-21 12:17:30 +0545

    Render blogsData and list

---
+++ src/components/Blogs.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container my-4">
+        <div v-for="blog in blogData" class="list-group">
+            <BlogPost @deleteBlog="handleDeleteBlog" :blog="blog" />
+        </div>
+    </div>
+</template>
+
+<script>
+import BlogPost from './BlogPost.vue'
+export default ({
+    props: {
+        blogData: {
+            type: Array,
+            reruired: true
+        }
+    },
+    components: {
+        BlogPost
+    },
+    methods: {
+        handleDeleteBlog(id) {
+            this.$emit('handleDeleteBlog', id)
+        }
+    }
+})
+</script>
