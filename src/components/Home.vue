5eb49e1686b61540bcf00e6a1caa6311924ac2da
Amrit <fenpadorje7@gmail.com>
2024-01-21 12:18:24 +0545

    Fetched Blog Data and created mini components.

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <Navbar />
+    <Blogs @handleDeleteBlog="handleDeleteBlog" :blogData="blogData" />
+</template>
+<script>
+import Navbar from './Navbar.vue';
+import Blogs from './Blogs.vue'
+export default ({
+    components: {
+        Navbar, Blogs
+    },
+    data() {
+        return {
+            url: 'http://localhost:3000/blogPosts',
+            blogData: []
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        async fetchData() {
+            const res = await fetch(this.url);
+            const data = await res.json()
+            return this.blogData = data;
+
+        },
+        handleDeleteBlog(id) {
+            this.blogData = this.blogData.filter(blog => blog.id !== id)
+
+        }
+    }
+})
+</script>
